25f83880e967a1bf0faa0ef29e4322b70c46c893
lazydok <soundlayerd@gmail.com>
2018-11-18 18:38:12 +0900

    initial

--- server.py
+++ server.py
@@ -1,61 +1,60 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restful import reqparse, abort, Api, Resource
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 api = Api(app)
+
 SEQ = 0
 
-applications = {
-    1: {'name': '', 'phone': 'build an API'},
+APPLICATIONS = {
+    1: {
+        'name': '',
+        'phone': '',
+        'email': '',
+        'content': '',
+    },
 }
 
 
-def abort_if_todo_doesnt_exist(todo_id):
-    if todo_id not in TODOS:
-        abort(404, message="Todo {} doesn't exist".format(todo_id))
+def abort_if_todo_doesnt_exist(app_id):
+    if app_id not in APPLICATIONS:
+        abort(404, message="APPLICATIONS {} doesn't exist".format(app_id))
 
 parser = reqparse.RequestParser()
-parser.add_argument('task')
+parser.add_argument('name')
+parser.add_argument('phone')
+parser.add_argument('email')
+parser.add_argument('content')
 
 
 # Todo
 # shows a single todo item and lets you delete a todo item
-class Todo(Resource):
-    def get(self, todo_id):
-        abort_if_todo_doesnt_exist(todo_id)
-        return TODOS[todo_id]
+class Apply(Resource):
+    # def get(self, todo_id):
+    #     abort_if_todo_doesnt_exist(todo_id)
+    #     return TODOS[todo_id]
+    #
+    # def delete(self, todo_id):
+    #     abort_if_todo_doesnt_exist(todo_id)
+    #     del TODOS[todo_id]
+    #     return '', 204
 
-    def delete(self, todo_id):
-        abort_if_todo_doesnt_exist(todo_id)
-        del TODOS[todo_id]
-        return '', 204
-
-    def put(self, todo_id):
+    def put(self):
         args = parser.parse_args()
-        task = {'task': args['task']}
-        TODOS[todo_id] = task
-        return task, 201
-
-
-# TodoList
-# shows a list of all todos, and lets you POST to add new tasks
-class TodoList(Resource):
-    def get(self):
-        return TODOS
-
-    def post(self):
-        args = parser.parse_args()
-        todo_id = int(max(TODOS.keys()).lstrip('todo')) + 1
-        todo_id = 'todo%i' % todo_id
-        TODOS[todo_id] = {'task': args['task']}
-        return TODOS[todo_id], 201
+        global SEQ
+        SEQ += 1
+        print(args)
+        args = {key: arg for key, arg in args.items()}
+        APPLICATIONS[SEQ] = args
+        print(APPLICATIONS)
+        return args, 201
 
 ##
 ## Actually setup the Api resource routing here
 ##
-api.add_resource(TodoList, '/todos')
-api.add_resource(Todo, '/todos/<todo_id>')
-
+api.add_resource(Apply, '/apply')
 
 if __name__ == '__main__':
     app.run(debug=True)
